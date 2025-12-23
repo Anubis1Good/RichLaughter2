e9f682d12195bbd51a22af750b0e3cd17ca16cb3
algoPVA <132759766+algoPVA@users.noreply.github.com>
2025-12-23 18:00:02 +0300

    Add files via upload

--- dev_indicators.py
+++ dev_indicators.py
@@ -2,18 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from utils.draw_funcs import draw_hb_chart_fast
-from indicators.classic_ind import add_fractals
-from indicators.pva_ind import add_nlevels_fractal
+from indicators.classic_ind import *
+from indicators.pva_ind import *
 
-# raw_file = 'data_for_tests\data_from_moex5\_5IMOEXF_1_1763448583.parquet'
-raw_file = 'data_for_tests\data_from_moex5\_5IMOEXF_1_1763893692.parquet'
+raw_file = 'data_for_tests\data_from_moex5\_5IMOEXF_1_1763448583.parquet'
+# raw_file = 'data_for_tests\data_from_moex5\_5IMOEXF_1_1763893692.parquet'
 df = pd.read_parquet(raw_file)
 
-
-df = add_fractals(df,10)
-
-    
-df = add_nlevels_fractal(df,10,0.5)
+# df = add_bollinger(df,20)
+# df['bbm'] = df['sma'].copy()
+# df = add_sma(df,5)
+# df['buff_bb'] = (df['bbu'] - df['bbd'])/2
+# df['top_line'] = df['sma'] + df['buff_bb']
+# df['bottom_line'] = df['sma'] - df['buff_bb']
+df = add_atr(df,5)
+df['prev_close'] = df['close'].shift(1)
+df['top_line'] = df['prev_close'] + df['atr'] * 1
+df['bottom_line'] = df['prev_close'] - df['atr'] * 1
         
 print(df.tail())
 if 0:
@@ -29,17 +34,13 @@ if 0:
     ax2.plot(df['atr'])
 else:
     draw_hb_chart_fast(df)
-    ups = df['fractal_up'] == True
-    downs = df['fractal_down'] == True
-    plt.scatter(df.loc[ups, 'x'], df.loc[ups, 'high'])
-    plt.scatter(df.loc[downs, 'x'], df.loc[downs, 'low'])
-    # print(df.iloc[-1].index)
-    for col in df.columns:
-        if 'top_' in col:
-            plt.plot(df[col],color='r')
-        if 'bot_' in col:
-            plt.plot(df[col],color='g')
-    # plt.plot(df['fractal_down'])
+    # plt.plot(df['bbu'],color='b')
+    # plt.plot(df['bbd'],color='b')
+    # plt.plot(df['bbm'],color='b')
+
+    # plt.plot(df['sma'],color='g')
+    plt.plot(df['top_line'],color='g')
+    plt.plot(df['bottom_line'],color='g')
 
 
 # Автоматическая регулировка layout'а
